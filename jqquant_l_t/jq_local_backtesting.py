c9e7725fe5d51f55ce30849438cdfa3b8d20a4cf
罗丙松 <luobs@huayunsoft.com>
2025-11-09 11:43:59 +0800

    1

--- jqquant_l_t/jq_local_backtesting.py
+++ jqquant_l_t/jq_local_backtesting.py
@@ -1,2 +1,15 @@
-from backtesting.test import GOOG
-print(GOOG.head())
+# from backtesting.test import GOOG
+
+# print(GOOG.head())
+
+from jqdatasdk import *
+
+auth('13877907589', 'aA*963.-+')
+
+print(get_account_info())
+
+print(get_query_count())
+
+print(get_price("601607.XSHG", start_date='2025-06-01', end_date='2025-06-10', frequency='1m'))
+
+print(get_query_count())
